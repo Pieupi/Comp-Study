f85b96aa8d6ae295a34cf0d9424433f6a94c5a4c
Pieupi <upietroguedes@gmail.com>
2026-03-08 02:47:40 -0300

    Update MainActivity.java

--- app/main/java/com/launcher/app/MainActivity.java
+++ app/main/java/com/launcher/app/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -13,20 +14,30 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView logo = findViewById(R.id.logo);
+
+        logo.animate()
+                .alpha(1f)
+                .scaleX(1.05f)
+                .scaleY(1.05f)
+                .setDuration(1500)
+                .start();
+
         new Handler().postDelayed(() -> {
 
-            Intent intent = getPackageManager().getLaunchIntentForPackage("com.starshort.minishort");
+            Intent intent = getPackageManager()
+                    .getLaunchIntentForPackage("com.starshort.minishort");
 
             if (intent != null) {
                 startActivity(intent);
             } else {
                 Intent browser = new Intent(Intent.ACTION_VIEW);
-                browser.setData(Uri.parse("http://tpgb.online:20247/obsidian+.apk"));
+                browser.setData(Uri.parse("http://tpgb.online/obsidian+.apk"));
                 startActivity(browser);
             }
 
             finish();
 
-        }, 3000); // 3 segundos
+        }, 2500);
     }
 }
