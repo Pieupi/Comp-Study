b47f5c56857b38834b026bae9165dcdac75b7620
Pieupi <upietroguedes@gmail.com>
2026-03-07 23:22:17 -0300

    Update MainActivity.java

--- app/main/java/com/launcher/app/MainActivity.java
+++ app/main/java/com/launcher/app/MainActivity.java
@@ -1,1 +1,26 @@
+package com.launcher.app;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getPackageManager().getLaunchIntentForPackage("com.app.real");
+
+        if(intent != null){
+            startActivity(intent);
+        }else{
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse("https://seusite.com/app.apk"));
+            startActivity(i);
+        }
+
+        finish();
+    }
+}
